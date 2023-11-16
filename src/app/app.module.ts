88e3ec10a0cb9d4a22736ac98a032c7c0d8b81b0
MathewAlcoseba <mathewalcoseba25@gmail.com>
2023-11-16 10:54:03 -0800

    added more info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+
+
+
+const routes: Routes =[
+
+  {path:'', component:HomepageComponent},
+
+
+];
 
 @NgModule({
   declarations: [
