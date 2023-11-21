360c256379b0b6e1708773ba02e25dc4bd4be698
MathewAlcoseba <mathewalcoseba25@gmail.com>
2023-11-21 10:58:32 -0800

    added input fields and buttons

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class HomepageComponent {
 
 }
+
+// app.ts
+
+// Function to handle scrolling to the specified element
+function scrollToElement(elementId: string): void {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+// Add an event listener to the "Flights" button
+document.addEventListener('DOMContentLoaded', () => {
+  const flightsButton = document.getElementById('flightsButton');
+
+  if (flightsButton) {
+    flightsButton.addEventListener('click', () => {
+      scrollToElement('bodyText1'); // Scroll to the bodyText1 section
+    });
+  }
+});
