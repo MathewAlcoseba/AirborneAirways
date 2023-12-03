90461dd0879c1713e345d8701abf8a9527c93090
AEG14 <gimenezaithan@gmail.com>
2023-12-03 17:17:18 +0800

    can now add flights to user

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -18,51 +18,14 @@ export class BookingComponent implements OnInit {
   selectedDestinationCountry: string = '';
   departureSearchQuery: string = '';
   destinationSearchQuery: string = '';
-  isRoundTripSelected: boolean = false;
+  isRoundTripSelected: boolean = true;
   filteredDepartureCountries: string[] = [];
   filteredDestinationCountries: string[] = [];
+  departureDate: string = '';
+  returnDate: string = '';
+  selectedTripType: string = 'Round-trip';
 
-  // countries: string[] = [
-  //   "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
-  //   "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas",
-  //   "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", 
-  //   "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil",
-  //   "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
-  //   "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China",
-  //   "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the",
-  //   "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-  //   "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",
-  //   "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia",
-  //   "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
-  //   "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
-  //   "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", 
-  //   "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", 
-  //   "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo",
-  //   "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
-  //   "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi",
-  //   "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania",
-  //   "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro",
-  //   "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands",
-  //   "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway",
-  //   "Oman", "Pakistan", "Palau", "Palestine State", "Panama", "Papua New Guinea",
-  //   "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania",
-  //   "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines",
-  //   "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", 
-  //   "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia",
-  //   "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan",
-  //   "Sudan, South", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan",
-  //   "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga",
-  //   "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu",
-  //   "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States",
-  //   "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam",
-  //   "Yemen", "Zambia", "Zimbabwe"
-  // ];
-  
-  // selectedDepartureCountry: string = '';
-  // selectedDestinationCountry: string = '';
-  // departureSearchQuery: string = '';
-  // destinationSearchQuery: string = '';
-  selectedTripType: string = 'Round-Trip';
+
 
   constructor(
     private authService: AuthService,
@@ -71,6 +34,7 @@ export class BookingComponent implements OnInit {
   ) { }
   
   ngOnInit(): void {
+    this.isUserLoggedIn = this.authService.isLoggedIn();
     // Fetch the list of origins from Firestore
     this.angularFirestore.collection('flights').valueChanges()
       .pipe(take(1))
@@ -80,20 +44,6 @@ export class BookingComponent implements OnInit {
       });
   }
 
-  // filterDepartureCountries(): void {
-  //   this.filteredDepartureCountries = this.departureSearchQuery ?
-  //     this.countries.filter(country =>
-  //       country.toLowerCase().includes(this.departureSearchQuery.toLowerCase())
-  //     ) : this.countries;
-
-  // }
-
-  // filterDestinationCountries(): void {
-  //   this.filteredDestinationCountries = this.destinationSearchQuery ?
-  //     this.countries.filter(country =>
-  //       country.toLowerCase().includes(this.destinationSearchQuery.toLowerCase())
-  //     ) : this.countries;
-  // }
 
   selectDepartureCountry(origin: string): void {
     this.selectedDepartureCountry = origin;
@@ -101,12 +51,16 @@ export class BookingComponent implements OnInit {
       .filter(flight => flight.origin === origin)
       .map(flight => flight.destination);
   
+    // Set the departureSearchQuery to the selected origin
+    this.departureSearchQuery = origin;
+  
     // Reset the selected destination country when a new departure country is selected
     this.selectedDestinationCountry = '';
     this.destinationSearchQuery = '';
     // Update the filtered destination countries based on the new destination options
     this.filterDestinationCountries();
   }
+  
   
 
   selectDestinationCountry(country: string): void {
@@ -136,17 +90,51 @@ export class BookingComponent implements OnInit {
 
   
   bookFlight(): void {
+    console.log('Is user logged in:', this.authService.isLoggedIn());
     if (this.authService.isLoggedIn()) {
-      if (this.isRoundTripSelected) {
-        // Implement logic for round-trip flight booking
-        // You can use this.selectedDepartureCountry and this.selectedDestinationCountry
-        // to get the selected departure and destination countries
-        alert('Round-trip flight booked!');
+      console.log('User UID:', this.authService.getUser().userId);
+
+      if (this.selectedDepartureCountry && this.selectedDestinationCountry) {
+        // Validate if the selected origin and destination exist in flights
+        const flight = this.flights.find(f =>
+          f.origin === this.selectedDepartureCountry &&
+          f.destination === this.selectedDestinationCountry
+        );
+  
+        console.log('Selected flight:', flight);
+        console.log('All flights:', this.flights);
+  
+        if (flight) {
+          // Calculate ticket price based on flight type
+          const ticketPrice = this.isRoundTripSelected
+            ? flight.ticketPrice * 2 * 0.9  // 10% discount for round-trip
+            : flight.ticketPrice;
+  
+          // Create userFlight object
+          const userFlight = {
+            flightId: flight.flightId,
+            origin: flight.origin,
+            destination: flight.destination,
+            flightType: this.isRoundTripSelected ? 'Round-trip' : 'One-way',
+            ticketPrice: ticketPrice,
+            departureDate: this.departureDate,
+            returnDate: this.isRoundTripSelected ? this.returnDate : null,
+          };
+          console.log('Selected flight:', userFlight);
+
+          // Use the custom userId property to get the user's document
+          const userId = this.authService.getUser().userId;
+          const userCollection = this.angularFirestore.collection('users').doc(userId);
+  
+          // Add userFlight to user's userFlights collection
+          userCollection.collection('userFlights').add(userFlight)
+            .then(() => alert('Flight booked successfully!'))
+            .catch(error => alert('Error booking flight: ' + error));
+        } else {
+          alert('Invalid flight selection. Please choose a valid origin and destination.');
+        }
       } else {
-        // Implement logic for one-way flight booking
-        // You can use this.selectedDepartureCountry and this.selectedDestinationCountry
-        // to get the selected departure and destination countries
-        alert('One-way flight booked!');
+        alert('Please select origin and destination before booking.');
       }
     } else {
       alert('Please log in to book a flight.');
@@ -154,8 +142,13 @@ export class BookingComponent implements OnInit {
     }
   }
 
+  
+
   onSelectTripType(option: string): void {
     this.selectedTripType = option;
+    this.isRoundTripSelected = option === 'Round-trip';
   }
   
+  
 }
+  
