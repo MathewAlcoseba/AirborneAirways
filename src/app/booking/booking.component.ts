316be3a384d97185ff462b4af857350b87eb2fc3
AEG14 <gimenezaithan@gmail.com>
2023-12-05 00:25:16 +0800

    display calculated total amount

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -3,7 +3,6 @@ import { AuthService } from '../auth.service';
 import { take } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-booking',
@@ -25,8 +24,7 @@ export class BookingComponent implements OnInit {
   departureDate: string = '';
   returnDate: string = '';
   selectedTripType: string = 'Round-trip';
-
-
+  ticketPrice: number = 0;
 
 
 
@@ -34,12 +32,10 @@ export class BookingComponent implements OnInit {
     private authService: AuthService,
     private router: Router,
     private angularFirestore: AngularFirestore,
-    // private sharedService: SharedService
   ) { }
   
   ngOnInit(): void {
     this.isUserLoggedIn = this.authService.isLoggedIn();
-    // Fetch the list of origins from Firestore
     this.angularFirestore.collection('flights').valueChanges()
       .pipe(take(1))
       .subscribe((flights: any[]) => {
@@ -48,9 +44,30 @@ export class BookingComponent implements OnInit {
       });
   }
 
+  calculateTicketPrice(): void {
+    if (this.selectedDepartureCountry && this.selectedDestinationCountry) {
+      // Find the selected flight
+      const flight = this.flights.find(f =>
+        f.origin === this.selectedDepartureCountry &&
+        f.destination === this.selectedDestinationCountry
+      );
+
+      if (flight) {
+        // Calculate ticket price based on flight type
+        this.ticketPrice = this.isRoundTripSelected ? flight.ticketPrice * 2 * 0.9 : flight.ticketPrice;
+      } else {
+        // Default price if no valid flight is found
+        this.ticketPrice = 0;
+      }
+    } else {
+      // Default price if either departure or destination is not selected
+      this.ticketPrice = 0;
+    }
+  }
 
   selectDepartureCountry(origin: string): void {
     this.selectedDepartureCountry = origin;
+    this.calculateTicketPrice();
     // this.sharedService.changeDepartureCountry(origin);
     this.destinationOptions = this.flights
       .filter(flight => flight.origin === origin)
@@ -73,6 +90,7 @@ export class BookingComponent implements OnInit {
     // this.sharedService.changeDestinationCountry(country); 
     this.destinationSearchQuery = country;
     // Additional logic if needed
+    this.calculateTicketPrice();
   }
 
   filterDepartureCountries(): void {
@@ -93,8 +111,6 @@ export class BookingComponent implements OnInit {
   onRadioChange(value: string): void {
     this.isRoundTripSelected = value === 'Round-trip';
   }
- 
-  
   
   bookFlight(): void {
     console.log('Is user logged in:', this.authService.isLoggedIn());
@@ -117,7 +133,11 @@ export class BookingComponent implements OnInit {
             ? flight.ticketPrice * 2 * 0.9  // 10% discount for round-trip
             : flight.ticketPrice;
   
-          // Create userFlight object
+          const seat = this.generateRandomSeat();
+          const gate = this.generateRandomGate();
+          const flightNumber = this.generateFlightNumber(flight.origin, flight.destination);
+
+
           const userFlight = {
             flightId: flight.flightId,
             origin: flight.origin,
@@ -126,14 +146,15 @@ export class BookingComponent implements OnInit {
             ticketPrice: ticketPrice,
             departureDate: this.departureDate,
             returnDate: this.isRoundTripSelected ? this.returnDate : null,
+            seat: seat,
+            gate: gate,
+            flightNumber: flightNumber
           };
           console.log('Selected flight:', userFlight);
 
-          // Use the custom userId property to get the user's document
           const userId = this.authService.getUser().userId;
           const userCollection = this.angularFirestore.collection('users').doc(userId);
   
-          // Add userFlight to user's userFlights collection
           userCollection.collection('userFlights').add(userFlight)
             .then(() => alert('Flight booked successfully!'))
             .catch(error => alert('Error booking flight: ' + error));
@@ -154,14 +175,27 @@ export class BookingComponent implements OnInit {
   onSelectTripType(option: string): void {
     this.selectedTripType = option;
     this.isRoundTripSelected = option === 'Round-trip';
+    this.calculateTicketPrice();
   }
   
-  // onDateChange(): void {
-  //   this.sharedService.changeDepartureDate(this.departureDate);
-  // }
-  // onReturnDateChange(): void {
-  //   this.sharedService.changeReturnDate(this.returnDate);
-  // }
-  
+  generateRandomSeat(): string {
+    const letters = 'ABCDEFG';
+    const randomLetter = letters[Math.floor(Math.random() * letters.length)];
+    const randomNumber = Math.floor(Math.random() * 25) + 1;
+    return randomLetter + randomNumber;
+  }
+
+  generateRandomGate(): number {
+    return Math.floor(Math.random() * 3) + 1;
+  }
+
+  generateFlightNumber(origin: string, destination: string): string {
+    const getConsonants = (str: string) => str.replace(/[aeiouAEIOU0-9]/g, '');
+    const originConsonants = getConsonants(origin).slice(0, 2);
+    const destinationConsonants = getConsonants(destination).slice(0, 2);
+    const randomNumbers = Math.floor(100 + Math.random() * 900); // 3 random digits
+    return originConsonants + destinationConsonants + randomNumbers;
+  }
+
 }
   
